87a77897df51c9a3d0c0e9f0e78a6245863d0636
Raju Sharma <141127259+pashupatitv@users.noreply.github.com>
2024-05-29 08:32:47 +0000

    variable defined

---
+++ 01-variables/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountCity = "Pokhara"
+
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = " kathmandu"

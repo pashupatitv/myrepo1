b261d9f7f061293312bbdd9d49ae3b103570d1a5
Raju Sharma <141127259+pashupatitv@users.noreply.github.com>
2024-05-29 08:38:14 +0000

    update

--- 01-variables/variables.js
+++ 01-variables/variables.js
@@ -1,11 +1,15 @@
 const accountId = 144553
 let accountEmail = "abc@google.com"
-var accountPassword = "12345"
+const accountPassword = "12345"
 accountCity = "Pokhara"
-
-
+let accountstate
+/*
+prefer to use let not var
+*/
 
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = " kathmandu"
+
+console.log([accountId]);
